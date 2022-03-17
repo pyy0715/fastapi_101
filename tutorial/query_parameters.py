056292faeca8f01e54a1fc3553dbfb8737dc1693
pyy0715 <zxc077@naver.com>
2022-03-17 16:48:05 +0900

    add query_parameters

---
+++ tutorial/query_parameters.py
@@ -0,0 +1,41 @@
+# uvicorn query_parameters:app --reload
+
+from typing import Optional
+from fastapi import FastAPI
+
+app = FastAPI()
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+## When you declare other function parameters that are not part of the path parameters, they are automatically interpreted as "query" parameters.
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+
+# Optional query_parameters
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: Optional[str] = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+
+# Multiple path and query parameters
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: Optional[str] = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
